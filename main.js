9ed1eec7741ba9fab4af40258e5a84865a588e6d
gingerzx <81630598+gingerzx@users.noreply.github.com>
2024-12-10 21:00:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -17,13 +17,13 @@ function createWindow() {
     win.loadFile('src/index.html');
     win.setMenuBarVisibility(false);
 
-    // Listen for the 'context-menu' event and send mouse position to renderer
+
     win.webContents.on('context-menu', (e, params) => {
-        // Send mouse position (x, y) to renderer process to display custom menu
+   
         win.webContents.send('show-custom-menu', params.x, params.y);
     });
 
-    // Go back to index.html from secret page
+  
     ipcMain.on('go-back', () => {
         win.loadFile('src/index.html');
     });
